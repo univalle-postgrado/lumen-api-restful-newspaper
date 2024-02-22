0eb65e6e53345c8305478221ecb2e8c11e808c36
Lucio Marcelo Quispe Ortega <marceloquispeortega@gmail.com>
2024-02-22 18:57:56 -0400

    Creación del modelo para Content

---
+++ app/Models/Content.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'pretitle',
+        'title',
+        'alias',
+        'author',
+        'image_url',
+        'introduction',
+        'body',
+        'tags',
+        'format',
+        'featured',
+        'status',
+        'edition_date',
+        'category_title',
+        'category_alias',
+        'created_at',
+        'updated_at',
+        'created_by',
+        'updated_by'
+    ];
+
+}
