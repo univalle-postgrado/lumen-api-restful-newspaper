d39a1929bec62db3fa10f9c07bf89af5ff06e57d
Lucio Marcelo Quispe Ortega <marceloquispeortega@gmail.com>
2024-02-23 09:50:05 -0400

    Creación del Model para Tag

---
+++ app/Models/Tag.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'title', 'alias', 'published', 'created_at', 'updated_at', 'created_by', 'updated_by'
+    ];
+
+}
